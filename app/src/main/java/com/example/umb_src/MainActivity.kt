6a5e31ad6d38b781a3ebb67beafff653d4f5234f
Wilson Alejandro Gonzalez Gaitan <wilsonalejandrogonzalezgaitan@MacBook-Air-de-Wilson.local>
2019-09-23 20:26:04 -0500

    Implement the webview

--- app/src/main/java/com/example/umb_src/MainActivity.kt
+++ app/src/main/java/com/example/umb_src/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.umb_src
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webView = findViewById<WebView>(R.id.container_web)
+        webView.settings.loadWithOverviewMode = true
+        webView.settings.useWideViewPort = true
+        webView.settings.builtInZoomControls = true
+        webView.webViewClient = myWebClient()
+        webView.loadUrl("http://192.168.0.12:8082/app-src/")
+
+    }
+}
+
+class myWebClient : WebViewClient() {
+
+    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+        view.loadUrl(url)
+        return true
+
     }
 }
