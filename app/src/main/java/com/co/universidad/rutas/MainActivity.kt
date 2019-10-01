c6dfdaca5cf23b0d109c3ca58ba67416dfe2477f
Sem - 6 - INGENIERIA INDUSTRIAL: Wilson Alejandro Gonzalez Gaitan <Lizethgio-2199@hotmail.com>
2019-09-30 21:31:13 -0500

    Refactor the code android

--- app/src/main/java/com/co/universidad/rutas/MainActivity.kt
+++ app/src/main/java/com/co/universidad/rutas/MainActivity.kt
@@ -1,10 +1,9 @@
-package rutas.umb.co.com.umb_src
+package com.co.universidad.rutas
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
-
-
+import rutas.umb.co.com.umb_src.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val myWebView = findViewById<WebView>(R.id.container_web_view)
-        myWebView.loadUrl("https://www.google.com")
+        val webSettings = myWebView.settings
+        webSettings.javaScriptEnabled = true
+        myWebView.loadUrl("http://192.168.0.19:4200/")
     }
 }
