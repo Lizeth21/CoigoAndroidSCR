b17c4c3f95bfb83520cb25ef19af192d0e689f81
ALEJGON <WILSON.GONZALEZ@ITAU.CO>
2019-10-01 16:12:55 -0500

    Implement the alert at webview

--- app/src/main/java/com/co/universidad/rutas/MainActivity.kt
+++ app/src/main/java/com/co/universidad/rutas/MainActivity.kt
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import rutas.umb.co.com.umb_src.R
+import android.webkit.WebChromeClient
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         val myWebView = findViewById<WebView>(R.id.container_web_view)
         val webSettings = myWebView.settings
         webSettings.javaScriptEnabled = true
+        myWebView.webChromeClient = WebChromeClient()
         myWebView.loadUrl("https://rutas-universidad.firebaseapp.com")
     }
 }
