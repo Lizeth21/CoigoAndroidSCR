0915ff9d0a9e27cfefa06314410ddaf6c5bb6260
Alejandro Gonzalez <lizethgio-2199@hotmail.com>
2019-11-03 23:26:49 -0500

    Implement the method to get gps data

--- app/src/main/java/com/co/universidad/rutas/MainActivity.kt
+++ app/src/main/java/com/co/universidad/rutas/MainActivity.kt
@@ -1,31 +1,29 @@
 package com.co.universidad.rutas
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebChromeClient
 import com.co.universidad.rutas.utilities.ManagePermissions
-import rutas.umb.co.com.umb_src.R
 import android.webkit.GeolocationPermissions
 import android.webkit.WebViewClient
-import android.support.v7.app.AlertDialog
-
 
 class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val myWebView = findViewById<WebView>(R.id.container_web_view)
-        myWebView.settings.javaScriptCanOpenWindowsAutomatically = true
-        myWebView.settings.builtInZoomControls = true
-        myWebView.webViewClient = GeoWebViewClient()
+        setContentView(rutas.umb.co.com.umb_src.R.layout.activity_main)
+        val myWebView = findViewById<WebView>(rutas.umb.co.com.umb_src.R.id.container_web_view)
         myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.javaScriptCanOpenWindowsAutomatically= true
+        myWebView.settings.setSupportZoom(false)
+        myWebView.settings.displayZoomControls = false
+        myWebView.settings.builtInZoomControls = false
         myWebView.settings.setGeolocationEnabled(true)
-        myWebView.webChromeClient = GeoWebChromeClient(this)
+        myWebView.webChromeClient = WebChromeClientSystem()
+        myWebView.webViewClient = WebViewClientSystem()
         myWebView.loadUrl("https://rutas-universidad.firebaseapp.com")
         this.getPermission()
     }
@@ -34,41 +32,20 @@ class MainActivity : AppCompatActivity() {
         val permission = arrayListOf<String>()
         permission.add((android.Manifest.permission.ACCESS_FINE_LOCATION))
         permission.add((android.Manifest.permission.ACCESS_COARSE_LOCATION))
+        permission.add((android.Manifest.permission.INTERNET))
         ManagePermissions(this, permission, 0).requestPermissions()
     }
 }
 
-class GeoWebViewClient : WebViewClient() {
+class WebViewClientSystem : WebViewClient() {
     override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
         view.loadUrl(url)
         return true
     }
 }
 
-/**
- * WebChromeClient subclass handles UI-related calls
- * Note: think chrome as in decoration, not the Chrome browser
- */
-class GeoWebChromeClient(private val context: Context) : WebChromeClient() {
-
-    override fun onGeolocationPermissionsShowPrompt(
-        origin: String,
-        callback: GeolocationPermissions.Callback
-    ) {
-        val remember = false
-        val builder = AlertDialog.Builder(context)
-        builder.setTitle("Locations")
-        builder.setMessage("Would like to use your Current Location ")
-            .setCancelable(true).setPositiveButton("Allow"
-            ) { _, _ ->
-                // origin, allow, remember
-                callback.invoke(origin, true, remember)
-            }.setNegativeButton("Don't Allow"
-            ) { _, _ ->
-                // origin, allow, remember
-                callback.invoke(origin, false, remember)
-            }
-        val alert = builder.create()
-        alert.show()
+class WebChromeClientSystem : WebChromeClient() {
+    override fun onGeolocationPermissionsShowPrompt(origin: String, callback: GeolocationPermissions.Callback) {
+        callback.invoke(origin, true, false)
     }
 }
